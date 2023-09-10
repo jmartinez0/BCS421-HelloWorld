88bdf7dd1559c51e1139ea83986faa31f495acb0
jmartinez0 <jmartinez.16c@gmail.com>
2023-09-09 23:23:20 -0400

    Hello World Commit

---
+++ app/src/main/java/com/example/bcs421helloworld/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.bcs421helloworld
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bcs421helloworld.ui.theme.BCS421HelloWorldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BCS421HelloWorldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("World")
+                }
+            }
+        }
+        println("Hello World");
+        Log.d("TestTag", "Hello World");
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BCS421HelloWorldTheme {
+        Greeting("World")
+    }
+}
